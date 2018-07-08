5cc7824568cd1bfa05286bda27b8dab5c60fdd10
Francisco Estanislao Rodríguez Lozano <festanislao.rl@gmail.com>
2018-07-07 23:36:37 -0500

    Nuevo mensaje

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Bienvenido</h1>
         </header>
         <p className="App-intro">
-          Para iniciar, edit <code>src/App.js</code> and save to reload.
+          Para iniciar, editando mucho <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
